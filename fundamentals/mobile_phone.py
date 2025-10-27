1cdaf4fd2ef97227b94acb5f032f769b749c2521
Oktavianus Misro Adrianto <oktavianuss.misro@gmail.com>
2025-10-27 11:49:47 +0700

    update code: the use of return and print; use loop on the caller

--- fundamentals/mobile_phone.py
+++ fundamentals/mobile_phone.py
@@ -3,19 +3,22 @@ class MobilePhone:
         self.number = number
     
     def turn_on(self):
-        return print(f'mobile phone {self.number} is turned on')
+        return f'mobile phone {self.number} is turned on'
     
     def turn_off(self):
-        return print('mobile phone is turned off')
+        return 'mobile phone is turned off'
 
     def call(self, number):
-        return print(f'calling {number}')
+        return f'calling {number}'
 
 okta = MobilePhone(number='081346803227')
 anjo = MobilePhone(number='085821012820')
 
-okta.turn_on()
-anjo.turn_on()
-okta.call(number='911')
-okta.turn_off()
-okta.turn_off()
+phones = [okta, anjo]
+for phone in phones:
+    print(phone.turn_on())
+
+print(phones[0].call(number='911'))
+
+for phone in phones:
+    print(phone.turn_off())
